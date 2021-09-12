91650d3f3e4e1bf14ee9b41eb2c7106002a721b8
unknown <ansumanrath54@gmail.com>
2021-09-12 12:50:55 +0530

    Pedometer Foreground

---
+++ android/app/build/generated/source/buildConfig/release/com/example/pedometer_foreground/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.pedometer_foreground;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.pedometer_foreground";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
---
+++ lib/main.dart
@@ -0,0 +1,211 @@
+import 'dart:isolate';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+import 'package:intl/intl.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:pedometer_foreground/notificationservice.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AndroidAlarmManager.initialize();
+  NotificationService().initNotification();
+
+  runApp(MyApp());
+}
+
+void startCallBack() {
+  FlutterForegroundTask.setTaskHandler(ForegroundTaskHandler());
+}
+
+void fireAlarm()  {
+  try {
+    NotificationService().initNotification();
+    NotificationService().showNotification(1, "title", "body");
+    print("hello");
+  }
+  catch(e) {
+    print(e);
+  }
+  //print('hello');
+}
+
+class ForegroundTaskHandler implements TaskHandler {
+
+  int count =0;
+  Stream<StepCount>? stepCount;
+  String steps = '?' , yesterdaySteps = '0';
+
+
+  @override
+  Future<void> onDestroy(DateTime timestamp) async {
+    await FlutterForegroundTask.clearAllData();
+  }
+
+  @override
+  Future<void> onEvent(DateTime timestamp, SendPort? sendPort) async {
+    count++;
+    if(count<=1) {
+      stepCount!.listen((event) {
+        steps = event.steps.toString();
+        FlutterForegroundTask.updateService(
+          notificationTitle: "Pedometer",
+          notificationText: "Total Steps: $steps     Yesterday Steps: $yesterdaySteps"
+              "\nTime: ${DateFormat("dd-MM-yyyy – kk:mm:ss").format(timestamp)}",
+          callback: null,
+        );
+      }, onError: (error) {
+        steps = "Step Count not Available";
+      },);
+    }
+
+    if((timestamp.hour == 00) && (timestamp.minute == 00) && (timestamp.second == 00)) {
+      int tSteps = int.parse(steps) - int.parse(yesterdaySteps);
+      yesterdaySteps = tSteps.toString();
+      print("Total Steps in a Day: $yesterdaySteps");
+    }
+  }
+
+  @override
+  Future<void> onStart(DateTime timestamp, SendPort? sendPort) async {
+    // if(await FlutterForegroundTask.isRunningService) {
+    //   return;
+    // }
+    stepCount = Pedometer.stepCountStream;
+  }
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _initForegroundTask() {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'notification_channel_id',
+        channelName: 'Foreground Notification',
+        channelDescription: 'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+        iconData: NotificationIconData(
+          resType: ResourceType.mipmap,
+          resPrefix: ResourcePrefix.ic,
+          name: 'launcher',
+        ),
+      ),
+      iosNotificationOptions: IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: ForegroundTaskOptions(
+        interval: 1000,
+        autoRunOnBoot: true,
+      ),
+      printDevLog: true,
+    );
+  }
+
+  void _startForegroundTask() async {
+    if(await FlutterForegroundTask.isRunningService) {
+      return;
+    }
+    await FlutterForegroundTask.startService(
+      notificationTitle: 'Foreground Service is running',
+      notificationText: 'Tap to return to the app',
+      callback: startCallBack
+    );
+  }
+
+  void _stopForegroundTask() {
+    FlutterForegroundTask.stopService();
+    AndroidAlarmManager.cancel(alarmId);
+  }
+
+  void initState() {
+    super.initState();
+    _initForegroundTask();
+    //tz.initializeTimeZones();
+  }
+
+  void dispose() {
+    super.dispose();
+  }
+
+  bool isOn = false;
+  int alarmId = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WithForegroundTask(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Foreground Task'),
+            centerTitle: true,
+          ),
+          body: _buildContentView(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContentView() {
+    final buttonBuilder = (String text, {VoidCallback? onPressed}) {
+      return ElevatedButton(
+        child: Text(text),
+        onPressed: onPressed,
+      );
+    };
+
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          buttonBuilder('START', onPressed: _startForegroundTask),
+          buttonBuilder('STOP', onPressed: _stopForegroundTask),
+          buttonBuilder('NOTIFY', onPressed: () {
+            AndroidAlarmManager.periodic(
+              Duration(seconds: 60), alarmId, fireAlarm,
+              exact: true,
+              wakeup: true,
+              rescheduleOnReboot: true
+            );
+            print("Triggered");
+            //fireAlarm();
+          },),
+          // SizedBox(height: 20,),
+          // Transform.scale(
+          //   scale: 2,
+          //   child: Switch(
+          //     value: isOn,
+          //     onChanged: (bool value) {
+          //       setState(() {
+          //         isOn = value;
+          //       });
+          //       if (isOn == true) {
+          //         AndroidAlarmManager.periodic(
+          //           Duration(seconds: 10), alarmId, fireAlarm,
+          //           startAt: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 11, 28),);
+          //         // AndroidAlarmManager.oneShotAt(DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 18, 43),
+          //         //     alarmId, fireAlarm, wakeup: true);
+          //       } else {
+          //         AndroidAlarmManager.cancel(alarmId);
+          //         print('Alarm Timer Canceled');
+          //       }
+          //     },
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
