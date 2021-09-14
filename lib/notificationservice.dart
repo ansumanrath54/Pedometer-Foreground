775979bb1d4e7a5a04a05623d3bac0506478e6d8
darshansatra1 <darshansatra1@gmail.com>
2021-09-14 19:15:27 +0530

    restructure the string

--- lib/notificationservice.dart
+++ lib/notificationservice.dart
@@ -1,29 +1,27 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  String _body = """Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.\n
-  Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment.\n
-  However, some will become seriously ill and require medical attention.\n
-  Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness.\n
-  Anyone can get sick with COVID-19 and become seriously ill or die at any age.""";
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  String _body =
+      """Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
+Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age.""";
 
   Future<void> initNotification() async {
     final AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('@drawable/ic_flutternotification');
+        AndroidInitializationSettings('@drawable/ic_flutternotification');
 
     final IOSInitializationSettings initializationSettingsIOS =
-    IOSInitializationSettings(
+        IOSInitializationSettings(
       requestAlertPermission: false,
       requestBadgePermission: false,
       requestSoundPermission: false,
     );
 
     final InitializationSettings initializationSettings =
-    InitializationSettings(
-        android: initializationSettingsAndroid,
-        iOS: initializationSettingsIOS
-    );
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
@@ -35,14 +33,11 @@ class NotificationService {
       body = _body,
       const NotificationDetails(
         android: AndroidNotificationDetails(
-            'main_channel',
-            'Main Channel',
-            'Main channel notifications',
+            'main_channel', 'Main Channel', 'Main channel notifications',
             importance: Importance.max,
             priority: Priority.max,
             icon: '@drawable/ic_flutternotification',
-            styleInformation: BigTextStyleInformation("")
-        ),
+            styleInformation: BigTextStyleInformation("")),
         iOS: IOSNotificationDetails(
           sound: 'default.wav',
           presentAlert: true,
